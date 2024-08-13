bc99f54c763f9ba7d358409a3c8fae9e4cde9af5
Jacob-biu <2777245228@qq.com>
2024-08-13 11:01:18 +0800

    细节更新

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,27 @@
  * @Author: Jacob-biu 2777245228@qq.com
  * @Date: 2024-08-07 21:52:26
  * @LastEditors: Jacob-biu 2777245228@qq.com
- * @LastEditTime: 2024-08-13 10:54:13
+ * @LastEditTime: 2024-08-13 11:01:05
  * @FilePath: \demo\llm_demo\src\App.vue
  * @Description: 
  * Copyright (c) 2024 by Jacob John, All Rights Reserved. 
 -->
 <template>
   <!-- <div class="background"></div> -->
-  <router-view/>
+  <!-- <router-view/> -->
   <p id="Sen">大模型AI 也可能会犯错哦。请核查重要信息。</p>
   <ChatDialog msg="LLM-demo"/>
 </template>
 
 <script>
-// import ChatDialog from './components/ChatDialog.vue'
+import ChatDialog from './components/ChatDialog.vue'
 
-// export default {
-//   name: 'App',
-//   components: {
-//     ChatDialog,
-//   }
-// }
+export default {
+  name: 'App',
+  components: {
+    ChatDialog,
+  }
+}
 </script>
 
 <style>

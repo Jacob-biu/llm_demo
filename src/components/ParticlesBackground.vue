1d72798b5d5bb6cf3efb7135b61bfa9ea4222729
Jacob-biu <2777245228@qq.com>
2024-09-03 17:21:35 +0800

    更新粒子束

--- src/components/ParticlesBackground.vue
+++ src/components/ParticlesBackground.vue
@@ -11,7 +11,11 @@ export default {
       ctx: null,
       particlesArray: [],
       count: 0,
-      DISTANCE_THRESHOLD: 100 // 控制连线的最大距离
+      DISTANCE_THRESHOLD: 100, // 控制连线的最大距离
+      mouse: {
+        x: 0,
+        y: 0
+      }
     };
   },
   mounted() {
@@ -20,6 +24,7 @@ export default {
     this.canvas.height = window.innerHeight;
     this.ctx = this.canvas.getContext('2d');
     this.count = Math.floor(this.canvas.height / 100 * this.canvas.width / 100 / 2);
+    this.addEventListeners();
     this.animate();
     window.addEventListener('resize', this.handleResize);
   },
@@ -27,15 +32,22 @@ export default {
     window.removeEventListener('resize', this.handleResize);
   },
   methods: {
+    addEventListeners() {
+      window.addEventListener('mousemove', this.handleMouseMove);
+    },
+    handleMouseMove(event) {
+      this.mouse.x = event.x;
+      this.mouse.y = event.y;
+    },
     createParticle() {
       const x = Math.random() * this.canvas.width;
       const y = Math.random() * this.canvas.height;
-      this.particlesArray.push(new Particle(x, y));
+      this.particlesArray.push(new Particle(x, y, this.mouse));
     },
     handleParticle() {
       for (let i = 0; i < this.particlesArray.length; i++) {
         const particle = this.particlesArray[i];
-        particle.update();
+        particle.update(this.mouse, this.canvas);
         particle.draw(this.ctx);
         if (particle.x < 0 || particle.x > this.canvas.width || particle.y < 0 || particle.y > this.canvas.height) {
           this.particlesArray.splice(i, 1);
@@ -74,20 +86,41 @@ export default {
   }
 };
 
+// 修改后的 Particle 类
 class Particle {
-  constructor(x, y) {
+  constructor(x, y, mouse) {
     this.x = x;
     this.y = y;
     this.directionY = 0.5 - Math.random();
     this.directionX = 0.5 - Math.random();
+    this.size = 2;
+    this.speed = 0.5; // 控制粒子向鼠标移动的速度
+    this.mouse = mouse;
   }
-  update() {
-    this.y += this.directionY;
-    this.x += this.directionX;
+  update(mouse, canvas) {
+    // 计算鼠标与粒子之间的距离
+    const dx = mouse.x - this.x;
+    const dy = mouse.y - this.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    // 调整速度使粒子朝向鼠标移动
+    if (distance < 100) {
+      this.x += (dx / distance) * this.speed;
+      this.y += (dy / distance) * this.speed;
+    } else {
+      this.x += this.directionX;
+      this.y += this.directionY;
+    }
+
+    // 边界处理
+    if (this.x < 0 || this.x > canvas.width || this.y < 0 || this.y > canvas.height) {
+      this.x = Math.random() * canvas.width;
+      this.y = Math.random() * canvas.height;
+    }
   }
   draw(ctx) {
     ctx.beginPath();
-    ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fillStyle = 'white';
     ctx.fill();
   }
